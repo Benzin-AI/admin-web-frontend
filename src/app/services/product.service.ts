34ab0bc24965d7f1d4bfe3ef2f39fa838472575e
Sergio Montenegro <benzin321@hotmail.com>
2024-09-03 09:01:10 -0300

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:3000/api/products';
+  private apiUrl = 'https://admin-web-backend.onrender.com/api/products';
 
   constructor(private http: HttpClient) { }
 
