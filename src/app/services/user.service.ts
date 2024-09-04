a18345475bf58e4c94af3f80623aa9b4b0d8dcc9
Sergio Montenegro <benzin321@hotmail.com>
2024-09-04 13:13:05 -0300

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, map, switchMap, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'https://admin-web-backend.onrender.com/api/users';
+  private apiUrl = 'http://localhost:3000/api/users';
 
   constructor(private http: HttpClient) { }
 
-  // Obtener el token del almacenamiento local
+  // Método para obtener las cabeceras con el token
   private getHeaders(): HttpHeaders {
     const token = localStorage.getItem('token');
     return new HttpHeaders().set('x-access-token', token ? token : '');
@@ -18,38 +18,37 @@ export class UserService {
 
   // Método para obtener todos los usuarios
   getUsers(): Observable<any> {
-    return this.http.get(this.apiUrl, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.get(this.apiUrl, { headers });
   }
 
   // Método para obtener un usuario por ID
   getUserById(id: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.get(`${this.apiUrl}/${id}`, { headers });
   }
 
   // Método para crear un nuevo usuario
   createUser(user: any): Observable<any> {
-    return this.http.post(this.apiUrl, user, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.post(this.apiUrl, user, { headers });
   }
 
   // Método para actualizar un usuario existente
   updateUser(id: string, user: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, user, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.put(`${this.apiUrl}/${id}`, user, { headers });
   }
 
   // Método para eliminar un usuario
   deleteUser(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers });
   }
 
+  // Método para obtener el usuario actual
   getCurrentUser(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/current`, { headers: this.getHeaders() });
+    const headers = this.getHeaders();
+    return this.http.get(`${this.apiUrl}/current`, { headers });
   }
-
-  private getToken(): string | null {
-    const token = localStorage.getItem('token');
-    return token;
-  }
-
-
-
 }
